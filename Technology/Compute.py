d4356df4035d4aaabb8c11084fafe1dd9c7273a4
Elisha Malinski <elishamalinski@gmail.com>
2024-12-09 19:27:56 -0600

    weeeeeeeeee

---
+++ Technology/Compute.py
@@ -0,0 +1,19 @@
+"""
+Resources you might find handy for this Compute section:
+
+All Compute products: https://aws.amazon.com/products/compute
+EC2 instance types: https://aws.amazon.com/ec2/instance-types
+EC2 instance purchasing options: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-purchasing-options.html
+PuTTY download (to SSH from Windows 8 or lower): https://www.putty.org
+"""
+
+"""EC2 (Elastic Compute Cloud)
+A virtual server that you rent in AWS called an "instance"
+
+- easy to scale
+- reliable
+- use with other services
+- only pay for what you use 
+
+
+"""
--- Technology/Technology.py
+++ Technology/Technology.py
@@ -26,3 +26,101 @@ AWS Global Infrastructure Map (regions, availability zones): https://aws.amazon.
 
 Edge Locations: https://aws.amazon.com/cloudfront/features/?whats-new-cloudfront.sort-by=item.additionalFields.postDateTime&whats-new-cloudfront.sort-order=desc
 """
+
+"""Cloud Deployment
+
+cloud ("public")
+    - Cost savings
+    - Scalability
+    - Flexibility
+    Infrastucture available to the genral public on the internet
+    Ownd by an organization selling cloud services 
+
+hybrid
+    comprised of two or more clouds (private, public)
+    Bound together to enable data and applications portability
+
+on-premises ("private")
+    - Privacy
+    - Total control
+    - Dedicated resources
+    Infrastucture solely for one organization
+    My be outsoursed or owned, on-premises or off-premises
+    Ownd by an organization selling on-premises services
+"""
+
+"""AWS Connectivity Options
+
+site-to-site VPN
+
+cloud <------------------> on-premises
+("public")                 ("private")
+
+-Encrypted
+-uses public internet
+-fast to set up
+
+
+AWS Direct Connect
+
+cloud <------------------> on-premises
+("public")                 ("private")
+
+-Dedicated physical connection
+- Bypasses publilc internet; uses dedicated privet network
+(not encrypted by default)
+-faster and more expensive
+-takes longer to set up
+"""
+
+"""AWS Infrastructure Concepts
+
+Availability Zone
+
+A single data center or group of data centers within a region,
+each with redundant power, networking and connectivity, housed in
+separate facilities.
+
+region (e.g., us-east-2)
+has 3 Availability Zones ant the time of writing
+
+Edge Location
+
+A site that amazon coudfront uses to store cached copies of your
+content closer to your customers for faster delivery.
+
+"""
+
+"""Important Points to Remember: Technology
+
+WAYS TO WORK WITH AWS
+    AWS Management Console (browser)
+    Programmatic access
+• From your local machine - both require access key IDs and secrets
+• Software Developer Kit (SDK)
+    Command Line Interface (CLI)
+• AWS CloudShell
+    Browser-based; doesn't require access key ID DEPLOYMENT MODELS
+    Public cloud
+    On-premises
+    Hybrid
+        Connectivity: Site-to-site VPN or direct connect
+
+Availability Zone
+
+A single data center or group of data centers within a region,
+each with redundant power, networking and connectivity, housed in
+separate facilities.
+
+region (e.g., us-east-2)
+has 3 Availability Zones ant the time of writing
+
+Edge Location
+
+A site that amazon coudfront uses to store cached copies of your
+content closer to your customers for faster delivery.
+
+- High availability = 2 or more AZs
+- withstand environmental desaster = deploy to 2 or more regions
+
+"""
